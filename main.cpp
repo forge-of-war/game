73ceafde1f9a4606fb1687a01db8ba4ba23a4ead
ruppenthal <leifruppenthal@gmx.de>
2015-02-05 00:22:33 +0100

    a

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world 243gn34gui43ng!" << endl;
+    cout << "Hello world 243gn34gui43ng!" << endl;
+    cout << "Hello world 243gn34gui43ng!" << endl;
     return 0;
 }
